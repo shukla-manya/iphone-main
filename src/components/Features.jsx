fe06f1b7e017a4f75aaac1a3a5f758bffa040d43
MANYA SHUKLA <work.shuklamanya@gmail.com>
2026-05-07 13:09:03 +0530

    Update Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,7 +3,6 @@ import React, { useRef } from 'react'
 import { animateWithGsap } from '../utils/animations';
 import { explore1Img, explore2Img, exploreVideo } from '../utils';
 import gsap from 'gsap';
-
 const Features = () => {
   const videoRef = useRef();
 
