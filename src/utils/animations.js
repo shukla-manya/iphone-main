801636dc7f1a1e6bd736a287d311d5e7d011e04e
MANYA SHUKLA <work.shuklamanya@gmail.com>
2026-07-10 13:43:01 +0530

    Update animations.js

--- src/utils/animations.js
+++ src/utils/animations.js
@@ -12,7 +12,6 @@ export const animateWithGsap = (target, animationProps, scrollProps) => {
     }
   })
 }
-
 export const animateWithGsapTimeline = (timeline, rotationRef, rotationState, firstTarget, secondTarget, animationProps) => {
   timeline.to(rotationRef.current.rotation, {
     y: rotationState,
